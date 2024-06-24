e44c030a0e62b76e5560404f6aa7482cb661aa87
sheyamvictor <sheyamjoseph@gmail.com>
2024-06-24 12:08:33 +0530

    updated all files

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import axios from "axios"
 
 
@@ -18,12 +18,28 @@ function Weather() {
 
     const [time, settime] = useState("")
 
+    const scrollRef = useRef(null);
+
+    const scrollToRight = () => {
+      if (scrollRef.current) {
+        scrollRef.current.scrollTo({
+          left: scrollRef.current.scrollWidth,
+          behavior: 'smooth' ,// Change to 'auto' for instant scroll,
+          duration:5000
+
+        });
+      }
+    };
+  
+    
+    
     function handelcity(evt) {
         setCity(evt.target.value)
     }
     function handelSearch() {
 
         var weatherdata = axios(`https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=95cb87010ac6cbd0577ef3cd0763778e`)
+
 
         weatherdata.then(function (success) {
             // console.log(success.data)
@@ -41,7 +57,7 @@ function Weather() {
         })
             
             
-        
+        scrollToRight();
 
     }
 
@@ -49,22 +65,21 @@ function Weather() {
 
     return (
        
-            <div className="bg-gradient-to-r from-cyan-100/30  to-blue-100/30 text backdrop-blur-sm p-20 px-10 border rounded-3xl m-5  border-r-purple-300 ">
-                <h1 className="text-3xl font-medium">Weather-Report</h1>
+            <div className="bg-gradient-to-r from-cyan-100/30  to-blue-100/30 text backdrop-blur-sm p-28 px-10 border rounded-3xl m-5 flex flex-wrap  border-r-purple-300 gap-4 justify-between">
+                <h1 className="text-3xl font-medium ">Weather-Report</h1>
                 <p className='font-medium'>I can give you a Weather 🌪️ report about your city :)</p>
-                <div className="flex m-5 justify-center gap-3">
+                <div className="flex flex-col m-2 p-4 justify-start items-center gap-3">
                     <input onChange={handelcity} type="text" placeholder='City?' className='mt-2  border border-green-400 rounded-md bg-transparent p-1 font-medium  hover:bg-green-200 text-black' required/>
-                    <button onClick={handelSearch} className='border border-green-400 p-1 mt-2 rounded-md  hover:bg-green-100 text-black '>Search</button>
+                    <button onClick={handelSearch} className='border border-green-400 p-1 mt-2 rounded-md  hover:bg-green-100 text-black '>Get Report</button>
                 </div>
-                <div className="flex justify-evenly gap-5 p-1 flex-wrap backdrop-opacity-10 backdrop-blur max-md:inline-grid  text-wrap">
-                    <h1 className="bg-gradient-to-r from-rose-300 to-blue-300 px-10 py-10 border rounded-md text-center text-2xl flex-grow"><b>🎌Country:</b>{country}</h1>
-                    <h1 className="bg-gradient-to-r from-purple-300 to-blue-400 px-10 py-10 border rounded-md text-center text-2xl flex-grow" ><b>🌆City:</b>{name}</h1>
-                    <h1 className="bg-gradient-to-r from-yellow-200 to-blue-400 px-10 py-10 border rounded-md text-center text-2xl flex-grow" ><b>🕛TimeZone:</b>{time}</h1>
-                    <h1 className="bg-gradient-to-r from-pink-300 to-blue-400 px-10 py-10 border rounded-md text-center text-2xl flex-grow" ><b>☁️Weather:</b>{weather}</h1>
-                    <h1 className="bg-gradient-to-r from-orange-300 to-blue-400 px-10 py-10 border rounded-md text-center text-2xl flex-grow" ><b>🌡️Temprature:</b>{temp}</h1>
-                    <h1 className="bg-gradient-to-r from-gray-300 to-blue-400 px-10 py-10 border rounded-md text-center text-2xl flex-grow" ><b>😶‍🌫️Description:</b>{desc}</h1>
+                    <h1 className="bg-gradient-to-r from-rose-300 to-blue-300 px-10 py-10 border rounded-md text-center text-2xl flex-grow shadow-md "><b>🎌</b>{country}</h1>
+                    <h1 className="bg-gradient-to-r from-purple-300 to-blue-400 px-10 py-10 border rounded-md text-center text-2xl flex-grow shadow-md" ><b>🌆</b>{name}</h1>
+                    <h1 className="bg-gradient-to-r from-yellow-200 to-blue-400 px-10 py-10 border rounded-md text-center text-2xl flex-grow shadow-md" ><b>🕛</b>{time}</h1>
+                    <h1 className="bg-gradient-to-r from-pink-300 to-blue-400 px-10 py-10 border rounded-md text-center text-2xl flex-grow shadow-md" ><b>☁️</b>{weather}</h1>
+                    <h1 className="bg-gradient-to-r from-orange-300 to-blue-400 px-10 py-10 border rounded-md text-center text-2xl flex-grow shadow-md" ><b>🌡️</b>{temp}</h1>
+                    <h1 className="bg-gradient-to-r from-gray-300 to-blue-400 px-10 py-10 border rounded-md text-center text-2xl flex-grow shadow-md" ><b>😶‍🌫️</b>{desc}</h1>
 
-                </div>
+                
 
 
             </div>
